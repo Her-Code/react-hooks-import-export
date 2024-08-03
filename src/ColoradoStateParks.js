7718542cfeb90640c235cf1c6b2bc8fd641ae305
+Sharon <+wanjiku.kahira@gmail.com>
2024-08-04 00:28:56 +0300

    add code to ColoradoStateParks.js

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,23 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import {trees, wildlife} from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
+}
+
+export default ColoradoStateParks;
+
+console.log(trees);
+wildlife();
